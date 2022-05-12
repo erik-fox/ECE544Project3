0df07bc55be4e3035b0a2259e62e1e87ff249512
erik-fox <erikfox589@gmail.com>
2022-05-12 14:16:27 -0700

    Add files via upload

--- pwm_generator.sv
+++ pwm_generator.sv
@@ -1,50 +1,34 @@
 //* pwm_generator.sv 
-//* Creates a pwm signal to an output pin based on a desired angle
-//* Also uses special servo commands for extra ability with servo control
-//* Requires registers at higher level for modification 
+//* Creates a pwm signal to an output pin based on a desired duty cycle 
 //**************************************
-module pwm_generator_top(
-	input  wire [31:0]  angle_requested,	//Used wire instead of logic due to issues on VGA lab
-    input  wire 		 reset,
-    input  wire 		 clk,
-    output logic	     PWMPIN_o	
+module pwm_generator(
+    input  logic 		    clock,
+    input  logic 		    reset,
+    input  logic    [31:0]  duty_cycle,	
+    output logic	        pwm_out	
 );
-    logic [31:0] counted_value;
-    logic [31:0] Duty_Cycle;
+    parameter       MAX_COUNT = 255;
+
+    logic   [31:0]  counter;
 	
-    // Convert the angle value to PWM Duty Cycle. 
-    //100MHZ clock, one clock cycle is 10ns
-	//[90 degree ranges, 2 to 1.66ms, 1.66 to 1.33ms, 1.33 to 1ms] [Left, Front, Right]1
-	//angle_requested(Duty_Cycle_) = 1ms + (1ms partition(20'd100000) / 180 degree) => Partitions/ 1 Degree => 5555 per degree(floor rounded down)
-	//Simplifying: angle_requested = 20'd1000000 + (5555*degree)
-	//Calculating in C rather than verilog due to floating point of angle
-	//Duty Cycle Reg written in address register of servo yaw(LeftRight) and pitch(updown)
-    always_comb begin
-        Duty_Cycle = angle_requested;
-    end
-	
-	
-	//Duty Cycle drive output high if the counter is within the range, else drive low 
-	always_comb begin
-		if (counted_value < Duty_Cycle) begin
-			PWMPIN_o = 1'b1;
-		end
-		else begin
-			PWMPIN_o = 1'b0;
-		end
-	end
-	
-	//Updates position every 20 ms
-	//Calculation for Servos
-	//Time Period of Signal is [(CLK:100MHz)/(1/(Refresh:20ms * 10^-3) = count required]
-	//20000000ns required for 20ms
-	//Clock ticks(counted) will be 20,000,000 / 10ns period = 2,000,000 count ticks
-	always_ff@(posedge clk) begin
-      if (reset == 1'b1 || counted_value == 31'd2000000) begin	//Period 20ms, clock  
-			counted_value <= 31'b0;
-		end
-		else begin
-			counted_value <= counted_value + 1'b1;
-		end
-	end
+
+	always_ff@(posedge clk) 
+        begin
+            if ( !reset || counter == MAX_COUNT ) 
+                begin	 
+			        counter <= '0;
+		        end
+		    else 
+                begin
+			        counter <= counter + 1'b1;
+                    if(counter < duty_cycle )
+                        begin
+                            pwm_out <= '1;
+                        end
+                    else
+                        begin
+                            pwm_out <= '0;
+                        end
+		        end
+	    end
 endmodule
