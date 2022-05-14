9fabda449d74120196317d2ee173ab5a79673715
Bewlar <57142363+Bewlar@users.noreply.github.com>
2022-05-13 21:15:17 -0700

    Updated C Code Major Revision
    Rewrote main C program
    Highlights:
    Switches working
    Green LEDS partially implemented
    Enc partially working
    OLED partially working
    PB partially working
    
    Todo List
    Pushbuttons
    	Decrement not working
    	Button incrementing debounce for infinite scaling, Rapidly changes PID
    OLED
    	Wiping vals
    ENC
    	Scale backwards not working, timing issue
    RPM
    	Limit high end just like low
    Watchdog implementation
    PID controller fix
    SSEG
    Green LEDs
    	Watchdog
    FreeRTOS Implementation

---
+++ Vitis/Project3_MBlaze_Platform/platform.tcl
@@ -0,0 +1,18 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct D:\PortlandState\ECE_544\Labs\project_3\Vitis\Project3_MBlaze_Platform\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source D:\PortlandState\ECE_544\Labs\project_3\Vitis\Project3_MBlaze_Platform\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {Project3_MBlaze_Platform}\
+-hw {D:\PortlandState\ECE_544\Labs\project_2\nexysA7fpga.xsa}\
+-proc {microblaze_0} -os {standalone} -fsbl-target {psu_cortexa53_0} -out {D:/PortlandState/ECE_544/Labs/project_3/Vitis}
+
+platform write
+platform generate -domains 
+platform active {Project3_MBlaze_Platform}
+platform generate
